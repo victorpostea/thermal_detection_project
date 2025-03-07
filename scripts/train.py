0ba02b24c2a768c8ba975a8d54ac5c2310148590
victorpostea <vpostea@uwo.ca>
2025-03-07 18:54:00 -0500

    fix requirements, make train script

---
+++ scripts/train.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+def train_yolo(data_yaml, epochs=50, model_type="yolov8n.pt"):
+    model = YOLO(model_type)
+    model.train(
+        data=data_yaml,
+        epochs=epochs,
+        imgsz=640,
+        batch=8,
+        name="thermal_yolo",
+    )
+
+if __name__ == "__main__":
+    train_yolo("../data/data.yaml", epochs=50, model_type="yolov8n.pt")
